2b49aca9541ec4b0c5ca12a019949191e71ebaec
Henry Burbridge <93581465+BUR073@users.noreply.github.com>
2023-09-12 16:40:01 +0100

    Update ML_Predictions.py

--- ML_Predictions.py
+++ ML_Predictions.py
@@ -4,8 +4,15 @@ from sklearn.tree import DecisionTreeClassifier
 # Load your data
 rtgData = pd.read_csv('tour_guide_data.csv')
 
-# Extract the features and target variable
-X = rtgData[['Temperature', 'Holiday']]  # Use double brackets to create a DataFrame
+# Convert 'Date' from string to datetime format
+rtgData['Date'] = pd.to_datetime(rtgData['Date'])
+
+# Convert 'Date' to the number of days since the first date in your dataset
+first_date = rtgData['Date'].min()
+rtgData['Date'] = (rtgData['Date'] - first_date).dt.days
+
+# Extract the features and target variable with explicit column naming
+X = pd.DataFrame(rtgData['Date'], columns=['Date'])
 y = rtgData['Number of river tour guides']
 
 # Create the model
@@ -14,8 +21,11 @@ model = DecisionTreeClassifier()
 # Fit the model to the data
 model.fit(X, y)
 
-# Make predictions
-new_data = [[-2, True], [64, False], [32, True]]
-new_data_df = pd.DataFrame(new_data, columns=['Temperature', 'Holiday'])
+# Convert new date to number of days since the first date
+new_data_date = pd.to_datetime('2024-01-03')
+days_since_first_date = (new_data_date - first_date).days
+
+# Make predictions using the converted date with explicit column naming
+new_data_df = pd.DataFrame([[days_since_first_date]], columns=['Date'])
 predictions = model.predict(new_data_df)
 predictions
